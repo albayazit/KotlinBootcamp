673368aa51aefc2db22251a48dd9fb0ccd858957
albayazit <bakhasanov@gmail.com>
2025-01-01 16:12:36 +0300

    T01. Task 6 - Sorting a collection by selection

---
+++ T01/ex6.kt
@@ -0,0 +1,42 @@
+import kotlin.system.exitProcess
+
+fun main() {
+    val listOfNumbers = getInput()
+    selectionSort(listOfNumbers)
+    println(listOfNumbers.joinToString(" "))
+}
+
+fun selectionSort(arr: MutableList<Double>) {
+    for (i in arr.indices) {
+        var minIndex = i
+        for (j in i until arr.size) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j
+            }
+        }
+        if (minIndex != i) {
+            val temp = arr[i]
+            arr[i] = arr[minIndex]
+            arr[minIndex] = temp
+        }
+    }
+}
+
+fun getInput(): MutableList<Double> {
+    while (true) {
+        try {
+            val size = readln().toInt()
+            if (size <= 0) {
+                println("Input error. Size <= 0")
+                exitProcess(1)
+            }
+            val numbers = readln().split(" ").map { it.toDouble() }
+            if (numbers.size != size) {
+                throw Exception()
+            }
+            return numbers.toMutableList()
+        } catch (e: Exception) {
+            println("Couldn\'t parse a number. Please, try again")
+        }
+    }
+}
